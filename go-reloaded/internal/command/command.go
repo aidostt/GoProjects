e843b40302712c1b6e27fd6f85ef36a9b6c31308
aidostt <leket1999@gmail.com>
2023-07-06 02:50:32 +0600

    Added handling advanced commands such as (up, 5); (low, 6); (cap, 2).

--- go-reloaded/internal/command/command.go
+++ go-reloaded/internal/command/command.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"errors"
 	"os"
 	"strings"
 )
@@ -21,6 +22,7 @@ func Check(file *os.File) ([]byte, error) {
 			case "(bin)":
 				words[i-1] = bin(words[i-1])
 			case "(up)":
+				//FIXME: adapt this case for new func
 				words[i-1] = strings.ToUpper(words[i-1])
 			case "(low)":
 				words[i-1] = strings.ToLower(words[i-1])
@@ -29,6 +31,48 @@ func Check(file *os.File) ([]byte, error) {
 			default:
 				return nil, ErrCommNotFound
 			}
+			words = delAtInd(words, i)
+		}
+	}
+	num := 0
+	for i, v := range words {
+		if _, exist := advCommands[v]; exist {
+			if words[i+1] == "" {
+				return nil, ErrInvalidInput
+			}
+			num, err = GetNum(words[i+1])
+			if err != nil {
+				switch {
+				case errors.Is(err, ErrInvalidInput):
+					return nil, ErrInvalidInput
+				default:
+					return nil, ErrCommNotFound
+				}
+			}
+			//if i+1 <= len(words) {
+			//	num, err = GetNum(words[i+1])
+			//	if err != nil {
+			//		return nil, ErrCommNotFound
+			//	}
+			//} else {
+			//	return nil, err
+			//}
+			switch v {
+			case "(up,":
+				for j := i; j >= i-num; j-- {
+					//FIXME: fix the index out of range error
+					words[j] = strings.ToUpper(words[j])
+				}
+			case "(low,":
+				for j := i; j >= i-num; j-- {
+					words[j] = strings.ToUpper(words[j])
+				}
+			case "(cap,":
+				for j := i; j >= i-num; j-- {
+					words[j] = strings.ToUpper(words[j])
+				}
+			}
+			words = delAtInd(words, i)
 			words = delAtInd(words, i)
 		}
 	}
--- go-reloaded/internal/command/utils.go
+++ go-reloaded/internal/command/utils.go
@@ -2,12 +2,15 @@ package command
 
 import (
 	"errors"
+	"regexp"
 	"strconv"
 )
 
 var (
 	commands        = map[string]string{"(hex)": "", "(bin)": "", "(up)": "", "(low)": "", "(cap)": ""}
+	advCommands     = map[string]string{"(up,": "", "(low,": "", "(cap,": ""}
 	ErrCommNotFound = errors.New("no such command")
+	ErrInvalidInput = errors.New("invalid input")
 )
 
 func hex(s string) string {
@@ -29,3 +32,25 @@ func bin(s string) string {
 func delAtInd(s []string, index int) []string {
 	return append(s[:index], s[index+1:]...)
 }
+
+func GetNum(s string) (output int, err error) {
+	//FIXME: 12d 1d2. Only acceptable input should be "...digit)"
+	expr := regexp.MustCompile(`\d+`)
+	match := expr.FindString(s)
+	if match == "" {
+		return 0, ErrInvalidInput
+	}
+	output, err = strconv.Atoi(match)
+	//if len(match) == 0 {
+	//	return 0, ErrInvalidInput
+	//}
+	//fmt.Println(match)
+	//output, err = strconv.Atoi(strings.Join(match, ""))
+	if err != nil {
+		return 0, err
+	}
+	return
+}
+
+//Example buzuk 30 books (cap,
+//1
